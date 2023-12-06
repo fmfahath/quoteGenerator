b486cfc07e63ea473f53a61ae2c990535d96dbda
fmfahath <fmfahath.dev@gmail.com>
2023-12-06 13:49:10 +0530

    js : fetched data displayed

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const quoteEl = document.getElementById('quote');
+const authorEl = document.getElementById('author');
 const api_url = 'https://api.quotable.io/random';
+
 
 async function getQuote(url){
     const response = await fetch(url);
     var data= await response.json();
     console.log(data);
+
+    quoteEl.innerHTML = data.content;
+    authorEl.innerHTML = data.author;
 }
 
 getQuote(api_url);
